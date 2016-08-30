b8869fe433e4b192a2a8e1ef84c17d9091a7dc36
Mahati Gollamudi <github email address>
2016-08-30 17:00:22 -0400

    if/else statments that ammend arrays via interpolation

---
+++ index.js
@@ -0,0 +1,37 @@
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+//pretty cool that my text editor knows what are array methods via color!
+
+
+function nowServing(line) {
+
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return `Currently serving ${line.shift()}.`
+  }
+
+}
+
+
+
+function currentLine(line) {
+
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+      var sentence = "The line is currently: "
+        for (var i = 0; i < line.length; i++) {
+        sentence = sentence.concat(`${i + 1}. ${line[i]}, `)
+        }
+  }
+  sentence = sentence.slice(0, -2);
+
+  return sentence
+
+}
